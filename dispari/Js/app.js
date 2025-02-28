efdfbd86573e93eff1dba4a8f9e78fe234d0a00a
martinolanza03 <lanzamartino3@gmail.com>
2025-02-28 17:39:18 +0100

    feat: add function sumEvenOrOdd and transform the user word to lowercase

--- dispari/Js/app.js
+++ dispari/Js/app.js
@@ -1,4 +1,5 @@
-const evenOrOdd = prompt('Inserisci pari se vuoi indovinare che il numero sia pari, altrimenti inserisci dispari');
+let evenOrOdd = prompt('Inserisci pari se vuoi indovinare che il numero sia pari, altrimenti inserisci dispari');
+evenOrOdd = evenOrOdd.toLocaleLowerCase();
 const userNumber = parseInt(prompt('Inserisci un numero da 1 a 5'));
 
 function randomNumber(){
@@ -8,3 +9,10 @@ function randomNumber(){
 
 const sumNumber = userNumber + randomNumber();
 
+function sumEvenOrOdd(sum) {
+    if(sum % 2 === 0){
+        return 'pari';
+    } else {
+        return 'dispari';
+    }
+}
